cc0b9e3a2a97f56f54ebe3a6ac910ec1cf1d6569
noxmore <76889683+noxmore@users.noreply.github.com>
2025-04-30 00:01:31 -0700

    Add `--workspace` flag to `cargo clippy` in CI

--- ci/full_check.sh
+++ ci/full_check.sh
@@ -4,9 +4,9 @@ set -x
 
 cargo fmt --all --check
 
-cargo clippy --no-default-features --all-targets
+cargo clippy --no-default-features --all-targets --workspace
 
-cargo clippy --all-features --all-targets
+cargo clippy --all-features --all-targets --workspace
 
 cargo test --all-features --all-targets
 cargo test --all-features --doc
