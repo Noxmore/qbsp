c1cb24d3ee85071297e3e781584e7f614144b2e5
Noxmore <76889683+noxmore@users.noreply.github.com>
2025-10-22 12:19:40 -0700

    Deprecate `BspData::format`

--- src/lib.rs
+++ src/lib.rs
@@ -253,6 +253,8 @@ pub fn read_mip_texture_lump(reader: &mut BspByteReader) -> BspResult<Vec<Option
 #[cfg_attr(feature = "bevy_reflect", derive(Reflect))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct BspData {
+	// TODO:
+	#[deprecated(note = "Accidentally included, will be removed in the next major release")]
 	pub format: BspFormat,
 	/// Essentially an embedded .map file, the differences being:
 	/// - Brush data has been stripped.
@@ -333,6 +335,7 @@ impl BspData {
 		let bspx = BspxData::new(bsp, &lump_dir.bspx).job("Reading BSPX data")?;
 
 		let data = Self {
+			#[expect(deprecated)]
 			format: ctx.format,
 			entities: std::str::from_utf8(&entities_bytes)
 				.map_err(BspParseError::map_utf8_error(&entities_bytes))
