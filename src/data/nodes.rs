a2b6efcff8674657f742a5486e3f118db4661308
Noxmore <76889683+noxmore@users.noreply.github.com>
2025-12-30 15:37:17 -0800

    Fix possible overflow in `BspNodeRef::from_*` and make fns const

--- src/data/nodes.rs
+++ src/data/nodes.rs
@@ -110,9 +110,10 @@ impl From<i16> for BspNodeRef<u16> {
 impl BspNodeRef<u32> {
 	/// Create a 32-bit `BspNodeRef` from an `i32`. Negative indices are treated as a leaf index
 	/// of `1 - signed_node_ref`. `-1` (leaf 0) means out-of-bounds.
-	pub fn from_i32(value: i32) -> Self {
+	pub const fn from_i32(value: i32) -> Self {
 		if value.is_negative() {
-			Self::Leaf((-value) as u32 - 1) // - 1 because you can't have -0
+			// We add one to deal with integer asymmetry. Using +1 instead of -1 to avoid possible overflow.
+			Self::Leaf(-(value + 1) as u32)
 		} else {
 			Self::Node(value as u32)
 		}
@@ -122,9 +123,10 @@ impl BspNodeRef<u32> {
 impl BspNodeRef<u16> {
 	/// Create a 16-bit `BspNodeRef` from an `i16`. Negative indices are treated as a leaf index
 	/// of `1 - signed_node_ref`. `-1` (leaf 0) means out-of-bounds.
-	pub fn from_i16(value: i16) -> Self {
+	pub const fn from_i16(value: i16) -> Self {
 		if value.is_negative() {
-			Self::Leaf((-value) as u16 - 1) // - 1 because you can't have -0
+			// We add one to deal with integer asymmetry. Using +1 instead of -1 to avoid possible overflow.
+			Self::Leaf(-(value + 1) as u16)
 		} else {
 			Self::Node(value as u16)
 		}
